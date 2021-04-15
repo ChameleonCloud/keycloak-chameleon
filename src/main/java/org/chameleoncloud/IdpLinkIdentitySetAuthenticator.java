113d5eb17abb5b8d699089422d8ff185d17d900a
Michael Sherman <shermanm@uchicago.edu>
2021-04-15 10:59:40 -0400

    add new authenticator to handle linked globus IDs
    This authenticator handles the case where a user creates a chameleon
    account with a globus identity, then later links that identity to a new
    primary.
    
    When authenticating, globus returns a token containing all associated,
    validated identities. If one of those identities matches the one linked
    to an existing chameleon user, it is safe to update the linked identity
    to the new one.
    
    This commit adds an authenticator to be used in the "broker first login"
    flow, and will update an existing user, if and only if an exact match is
    found for the linked identity.
    
    Since keycloak cannot handle multiple copies of an IdP linked to a user,
    we must delete the linked identity, then re-link the new one.
    This is done by attaching the found user to the authentication session,
    then returning success.
    
    To avoid orphaning accounts, the deletion and linking must happen
    atomically. For example, if a confirmation step was required to link,
    the deletion could happen without the relinking.
    
    TODO: The correct way to handle this is to attach an "ExistingUserInfo"
    object to the authentication session state, and then a later handler
    should handle the conflicts. When implemented, this authenticator will
    only handle detection/matching, and the delete+link functions should be
    moved to the confirmation step.

---
+++ src/main/java/org/chameleoncloud/IdpLinkIdentitySetAuthenticator.java
@@ -0,0 +1,105 @@
+package org.chameleoncloud;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jboss.logging.Logger;
+import java.util.List;
+import java.util.Map;
+
+import org.chameleoncloud.representations.GlobusIdentity;
+
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.authenticators.broker.AbstractIdpAuthenticator;
+import org.keycloak.authentication.authenticators.broker.util.SerializedBrokeredIdentityContext;
+
+import org.keycloak.broker.oidc.OIDCIdentityProvider;
+import org.keycloak.broker.provider.BrokeredIdentityContext;
+
+import org.keycloak.models.FederatedIdentityModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.KeycloakSession;
+
+import org.keycloak.representations.JsonWebToken;
+
+import org.keycloak.sessions.AuthenticationSessionModel;
+
+public class IdpLinkIdentitySetAuthenticator extends AbstractIdpAuthenticator {
+
+    String IDENTITY_SET_CLAIM = "identity_set";
+    // String GLOBUS_ALIAS = "globus";
+
+    private static Logger logger = Logger.getLogger(IdpLinkIdentitySetAuthenticator.class);
+
+    @Override
+    protected void authenticateImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx,
+            BrokeredIdentityContext brokerContext) {
+
+        KeycloakSession session = context.getSession();
+        RealmModel realm = context.getRealm();
+        AuthenticationSessionModel authSession = context.getAuthenticationSession();
+        String providerId = brokerContext.getIdpConfig().getAlias();
+
+        List<GlobusIdentity> identitySet = getIdentitiesFromToken(brokerContext);
+
+        FederatedIdentityModel newIdentity = new FederatedIdentityModel(providerId, brokerContext.getId(),
+                brokerContext.getUsername(), brokerContext.getToken());
+
+        for (GlobusIdentity identity : identitySet) {
+            // create model from identity parameters
+            FederatedIdentityModel oldIdentity = new FederatedIdentityModel(providerId, identity.getSub(),
+                    identity.getUsername(), brokerContext.getToken());
+            logger.warnv("Checking for existing users with {0} sub matching {1}", oldIdentity.getIdentityProvider(),
+                    oldIdentity.getUserId());
+
+            UserModel federatedUser = session.users().getUserByFederatedIdentity(oldIdentity, realm);
+            if (federatedUser != null) {
+                logger.warnv("Username {0} has existing link with provider {1}, removing linked id {2}",
+                        federatedUser.getUsername(), providerId, oldIdentity.getUserId());
+                // TODO: Handle duplicate globus case
+                // Link existing user to this token
+                // session.users().updateFederatedIdentity(realm, federatedUser, newIdentity);
+                session.users().removeFederatedIdentity(realm, federatedUser, providerId);
+                context.setUser(federatedUser);
+                context.success();
+                return;
+            } else {
+                logger.warnv("No match found for {0} username {1}", oldIdentity.getIdentityProvider(),
+                        oldIdentity.getUserName());
+            }
+        }
+
+        logger.warnv("No match in identity set for found for {0} username {1}", providerId,
+                brokerContext.getUsername());
+        context.attempted();
+
+        // TODO match on username / email of existing accounts from identityset
+
+    }
+
+    private List<GlobusIdentity> getIdentitiesFromToken(BrokeredIdentityContext brokerContext) {
+        JsonWebToken token = (JsonWebToken) brokerContext.getContextData().get(OIDCIdentityProvider.VALIDATED_ID_TOKEN);
+        Map<String, Object> otherClaims = token.getOtherClaims();
+        ObjectMapper mapper = new ObjectMapper();
+        List<GlobusIdentity> identity_set = mapper.convertValue(otherClaims.get(this.IDENTITY_SET_CLAIM),
+                new TypeReference<List<GlobusIdentity>>() {
+                });
+        return identity_set;
+    }
+
+    @Override
+    protected void actionImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx,
+            BrokeredIdentityContext brokerContext) {
+        authenticateImpl(context, serializedCtx, brokerContext);
+    }
+
+    @Override
+    public boolean requiresUser() {
+        return false;
+    }
+
+    @Override
+    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+        return false;
+    }
+}
