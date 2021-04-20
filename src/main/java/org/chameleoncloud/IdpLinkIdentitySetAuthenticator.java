8357e92c9236f51faa684e1ba66e81e00cb559e3
Michael Sherman <shermanm@uchicago.edu>
2021-04-20 19:38:41 -0400

    get logs to print...

--- src/main/java/org/chameleoncloud/IdpLinkIdentitySetAuthenticator.java
+++ src/main/java/org/chameleoncloud/IdpLinkIdentitySetAuthenticator.java
@@ -2,38 +2,27 @@ package org.chameleoncloud;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import org.jboss.logging.Logger;
 import java.util.List;
 import java.util.Map;
 
 import org.chameleoncloud.representations.GlobusIdentity;
 
-import org.jboss.logging.Logger;
-
-import org.keycloak.authentication.actiontoken.idpverifyemail.IdpVerifyAccountLinkActionToken;
-
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.authenticators.broker.AbstractIdpAuthenticator;
-import org.keycloak.authentication.authenticators.broker.IdpAutoLinkAuthenticator;
-import org.keycloak.authentication.authenticators.broker.IdpConfirmLinkAuthenticator;
-import org.keycloak.authentication.authenticators.broker.IdpEmailVerificationAuthenticator;
-import org.keycloak.authentication.authenticators.broker.IdpCreateUserIfUniqueAuthenticator;
-import org.keycloak.authentication.authenticators.broker.util.ExistingUserInfo;
 import org.keycloak.authentication.authenticators.broker.util.SerializedBrokeredIdentityContext;
 
 import org.keycloak.broker.oidc.OIDCIdentityProvider;
 import org.keycloak.broker.provider.BrokeredIdentityContext;
-import org.keycloak.broker.provider.IdentityProvider;
 
 import org.keycloak.models.FederatedIdentityModel;
-import org.keycloak.models.IdentityProviderModel;
-import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
-
+import org.keycloak.models.RealmModel;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.sessions.AuthenticationSessionModel;
 
 import org.keycloak.representations.JsonWebToken;
+
+import org.keycloak.sessions.AuthenticationSessionModel;
 
 public class IdpLinkIdentitySetAuthenticator extends AbstractIdpAuthenticator {
 
@@ -60,12 +49,12 @@ public class IdpLinkIdentitySetAuthenticator extends AbstractIdpAuthenticator {
             // create model from identity parameters
             FederatedIdentityModel oldIdentity = new FederatedIdentityModel(providerId, identity.getSub(),
                     identity.getUsername(), brokerContext.getToken());
-            logger.debugf("Checking for existing users with '%s' sub matching '%s'", oldIdentity.getIdentityProvider(),
+            logger.warnv("Checking for existing users with {0} sub matching {1}", oldIdentity.getIdentityProvider(),
                     oldIdentity.getUserId());
 
             UserModel federatedUser = session.users().getUserByFederatedIdentity(oldIdentity, realm);
             if (federatedUser != null) {
-                logger.debugf("Username '%s' has existing link with provider '%s'", federatedUser.getUsername(),
+                logger.warnv("Username {0} has existing link with provider {1}", federatedUser.getUsername(),
                         providerId);
                 // TODO: Handle duplicate globus case
                 // Link existing user to this token
@@ -74,12 +63,12 @@ public class IdpLinkIdentitySetAuthenticator extends AbstractIdpAuthenticator {
                 context.success();
                 return;
             } else {
-                logger.debugf("No match found for '%s' username '%s'", oldIdentity.getIdentityProvider(),
+                logger.warnv("No match found for {0} username {1}", oldIdentity.getIdentityProvider(),
                         oldIdentity.getUserName());
             }
         }
 
-        logger.debugf("No match in identity set for found for '%s' username '%s'", providerId,
+        logger.warnv("No match in identity set for found for {0} username {1}", providerId,
                 brokerContext.getUsername());
         context.attempted();
 
