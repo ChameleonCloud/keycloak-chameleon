35a20991c3942242ec00d133a059b682df538e9a
Michael Sherman <shermanm@uchicago.edu>
2021-04-21 12:48:02 -0400

    check for username,email matches in identitySet
    This adds checks for matching username and email, not just linked
    identity. However, we cannot assume that a match means the user is
    authenticated in this case, and an additional confirmation step is
    needed.
    
    Therefore, in these cases, we set "ExistingUserInfo" to the
    authentication context, and set context.attempted. This delegates
    conflict handling to later authenticators in the flow.
    
    A "sane" flow to handle this can be seen in the default "broker first
    login" flow, under "handle existing account". It first prompts the user
    to confirm account linking, then emails the listed contact email on the
    account for confirmation.
    
    TODO:
    The case that is currently unhandled is if a user authenticates with a
    new globus account, but alreday has one configured. The
    verifyEmailAuthenticator must be modified to handle the duplicate IDP entry.

--- src/main/java/org/chameleoncloud/IdpLinkIdentitySetAuthenticator.java
+++ src/main/java/org/chameleoncloud/IdpLinkIdentitySetAuthenticator.java
@@ -10,6 +10,7 @@ import org.chameleoncloud.representations.GlobusIdentity;
 
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.authenticators.broker.AbstractIdpAuthenticator;
+import org.keycloak.authentication.authenticators.broker.util.ExistingUserInfo;
 import org.keycloak.authentication.authenticators.broker.util.SerializedBrokeredIdentityContext;
 
 import org.keycloak.broker.oidc.OIDCIdentityProvider;
@@ -17,6 +18,7 @@ import org.keycloak.broker.provider.BrokeredIdentityContext;
 
 import org.keycloak.models.FederatedIdentityModel;
 import org.keycloak.models.UserModel;
+import org.keycloak.models.UserProvider;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.KeycloakSession;
 
@@ -38,42 +40,76 @@ public class IdpLinkIdentitySetAuthenticator extends AbstractIdpAuthenticator {
         KeycloakSession session = context.getSession();
         RealmModel realm = context.getRealm();
         AuthenticationSessionModel authSession = context.getAuthenticationSession();
-        String providerId = brokerContext.getIdpConfig().getAlias();
 
-        List<GlobusIdentity> identitySet = getIdentitiesFromToken(brokerContext);
+        String providerId = brokerContext.getIdpConfig().getAlias();
+        UserProvider cachedUsers = session.users();
 
         FederatedIdentityModel newIdentity = new FederatedIdentityModel(providerId, brokerContext.getId(),
                 brokerContext.getUsername(), brokerContext.getToken());
 
+        List<GlobusIdentity> identitySet = getIdentitiesFromToken(brokerContext);
         for (GlobusIdentity identity : identitySet) {
-            // create model from identity parameters
-            FederatedIdentityModel oldIdentity = new FederatedIdentityModel(providerId, identity.getSub(),
-                    identity.getUsername(), brokerContext.getToken());
-            logger.warnv("Checking for existing users with {0} sub matching {1}", oldIdentity.getIdentityProvider(),
-                    oldIdentity.getUserId());
 
-            UserModel federatedUser = session.users().getUserByFederatedIdentity(oldIdentity, realm);
+            // Check linked identities for match. If found, overwrite to prevent DB error
+            // This match implies that the broker is already authenticated for that
+            // identity, so this is safe without confirmation.
+            UserModel federatedUser = getUserByGlobusID(identity, cachedUsers, providerId, realm);
             if (federatedUser != null) {
-                logger.warnv("Username {0} has existing link with provider {1}, removing linked id {2}",
-                        federatedUser.getUsername(), providerId, oldIdentity.getUserId());
-                // TODO: Handle duplicate globus case
-                // Link existing user to this token
-                // session.users().updateFederatedIdentity(realm, federatedUser, newIdentity);
-                session.users().removeFederatedIdentity(realm, federatedUser, providerId);
+                // Overwrite linked provider ID with replacement
+                cachedUsers.removeFederatedIdentity(realm, federatedUser, providerId);
                 context.setUser(federatedUser);
                 context.success();
                 return;
-            } else {
-                logger.warnv("No match found for {0} username {1}", oldIdentity.getIdentityProvider(),
-                        oldIdentity.getUserName());
             }
+
+            // Check keycloak username for match, and set flag for later handling
+            UserModel userByUsername = cachedUsers.getUserByUsername(identity.getUsername(), realm);
+            if (userByUsername != null) {
+                ExistingUserInfo existingUser = new ExistingUserInfo(userByUsername.getId(), UserModel.USERNAME,
+                        userByUsername.getUsername());
+                authSession.setAuthNote(EXISTING_USER_INFO, existingUser.serialize());
+                context.attempted();
+                return;
+            }
+
+            // Check keycloak email for match, and set flag for later handling
+            // Only run if email present, and emails are unique
+            if (identity.getEmail() != null && !realm.isDuplicateEmailsAllowed()) {
+                UserModel userByEmail = cachedUsers.getUserByEmail(identity.getEmail(), realm);
+                if (userByEmail != null) {
+                    ExistingUserInfo existingUser = new ExistingUserInfo(userByEmail.getId(), UserModel.EMAIL,
+                            userByEmail.getEmail());
+                    authSession.setAuthNote(EXISTING_USER_INFO, existingUser.serialize());
+                    context.attempted();
+                    return;
+                }
+            }
+
         }
 
         logger.warnv("No match in identity set for found for {0} username {1}", providerId,
                 brokerContext.getUsername());
         context.attempted();
+    }
 
-        // TODO match on username / email of existing accounts from identityset
+    private UserModel getUserByGlobusID(GlobusIdentity identity, UserProvider cachedUsers, String providerId,
+            RealmModel realm) {
+
+        // create model from identity parameters
+        FederatedIdentityModel oldIdentity = new FederatedIdentityModel(providerId, identity.getSub(),
+                identity.getUsername());
+        logger.warnv("Checking for existing users with {0} sub matching {1}", oldIdentity.getIdentityProvider(),
+                oldIdentity.getUserId());
+        UserModel federatedUser = cachedUsers.getUserByFederatedIdentity(oldIdentity, realm);
+        if (federatedUser != null) {
+            logger.warnv("Username {0} has existing link with provider {1}, removing linked id {2}",
+                    federatedUser.getUsername(), providerId, oldIdentity.getUserId());
+            return federatedUser;
+        } else {
+            logger.warnv("No match found for {0} username {1}", oldIdentity.getIdentityProvider(),
+                    oldIdentity.getUserName());
+        }
+        return null;
 
     }
 
